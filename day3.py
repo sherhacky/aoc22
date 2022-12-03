65facf1fc0e76d9c29fbf6a7e59e5bb1556508ad
Sherwood Hachtman <shachtman@raybeam.com>
2022-12-02 21:50:49 -0800

    Clean debug line

--- day3.py
+++ day3.py
@@ -6,20 +6,19 @@ rucksacks = data.split('\n')[:-1]
 
 letters = string.ascii_lowercase + string.ascii_uppercase
 rank = {char: i+1 for i,char in enumerate(letters)}
-print(rank)
 
+# part 1
 total = 0
 for inventory in rucksacks:
     mid = len(inventory)//2
     left, right = inventory[:mid], inventory[mid:]
     common_item = (set(left) & set(right)).pop()
     total += rank[common_item]
-
 print(total)
 
+# part 2
 total = 0
 for i in range(0, len(rucksacks), 3):
     common_badge = (set(rucksacks[i]) & set(rucksacks[i+1]) & set(rucksacks[i+2])).pop()
     total += rank[common_badge]
-
 print(total)

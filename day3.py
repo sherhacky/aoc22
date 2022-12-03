83007fe31f2c9c82977dce0bdee237fcc56eaa45
Sherwood Hachtman <shachtman@raybeam.com>
2022-12-02 21:48:07 -0800

    Day 3

---
+++ day3.py
@@ -0,0 +1,25 @@
+import string
+
+with open('./input3.txt') as f:
+    data = f.read()
+rucksacks = data.split('\n')[:-1]
+
+letters = string.ascii_lowercase + string.ascii_uppercase
+rank = {char: i+1 for i,char in enumerate(letters)}
+print(rank)
+
+total = 0
+for inventory in rucksacks:
+    mid = len(inventory)//2
+    left, right = inventory[:mid], inventory[mid:]
+    common_item = (set(left) & set(right)).pop()
+    total += rank[common_item]
+
+print(total)
+
+total = 0
+for i in range(0, len(rucksacks), 3):
+    common_badge = (set(rucksacks[i]) & set(rucksacks[i+1]) & set(rucksacks[i+2])).pop()
+    total += rank[common_badge]
+
+print(total)

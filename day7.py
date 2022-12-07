7fedd122ff390151141584c71d6152d06678e0cc
Sherwood Hachtman <shachtman@raybeam.com>
2022-12-07 09:47:33 -0800

    outermost dir bugfix, readability/vanity

--- day7.py
+++ day7.py
@@ -10,8 +10,7 @@ filepaths = dict()
 # Using the dir tree walk, construct a dict of filepaths->sizes
 i = 0
 while i < len(rows):
-    row = rows[i]
-    chunks = row.split(' ')
+    chunks = rows[i].split(' ')
     if chunks[0] == '$' and chunks[1] == 'cd':
         if chunks[2] == '..':
             stack.pop()
@@ -33,12 +32,12 @@ dir_sizes = collections.defaultdict(int)
 # iterate through filepath sizes to determine directory sizes
 for item in filepaths:
     subdirs = item.split('/')[1:]
-    for i in range(len(subdirs)):
+    for i in range(1, len(subdirs)):
         dir_sizes['/'.join(subdirs[:i])] += filepaths[item]
 
 # part 1
 print(sum(size for size in dir_sizes.values() if size <= 100000))
 
 # part 2
-space_used = sum(filepaths.values())
-print(min(size for size in dir_sizes.values() if 70000000 - space_used + size >= 30000000))
+min_size_needed = dir_sizes[''] + 30000000 - 70000000
+print(min(size for size in dir_sizes.values() if size > min_size_needed))

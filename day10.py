ef793495b141110f5ef43b1c3bcb23d320895a98
Sherwood Hachtman <shachtman@raybeam.com>
2022-12-09 22:01:19 -0800

    Use strings for CRT

--- day10.py
+++ day10.py
@@ -3,13 +3,12 @@ with open('./input10.txt') as f:
 rows = data.split('\n')[:-1]
 
 # part 1
-
 total_signal_strength = 0
 x = 1
 cycle = 0
 for line in rows:
-    repeat = 1 + int(line[0] == 'a')
-    for _ in range(repeat):
+    iters = 1 + int(line[0] == 'a')
+    for _ in range(iters):
         cycle += 1
         if cycle % 40 == 20:
             total_signal_strength += x*cycle
@@ -19,21 +18,17 @@ for line in rows:
 print(total_signal_strength)
 
 # part 2
-
-screen = []
-scanline = []
+screen = ''
 x = 1
 cycle = 0
 for line in rows:
-    repeat = 1 + int(line[0] == 'a')
-    for _ in range(repeat):
-        scanline.append('#' if abs(x - cycle % 40) <= 1 else '.')
+    iters = 1 + int(line[0] == 'a')
+    for _ in range(iters):
+        screen += '#' if abs(x - cycle % 40) <= 1 else '.'
         cycle += 1
         if cycle % 40 == 0:
-            screen.append(scanline)
-            scanline = []
+            screen += '\n'
     n = int(line.split(' ')[1]) if line[0] == 'a' else 0
     x += n
 
-for scanline in screen:
-    print(''.join(scanline))
+print(screen)

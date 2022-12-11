f99796a5c2e911b044217adcf32aa91e438208c7
Sherwood Hachtman <shachtman@raybeam.com>
2022-12-11 12:13:36 -0800

    Combine parts and rename for readability

--- day10.py
+++ day10.py
@@ -1,34 +1,25 @@
 with open('./input10.txt') as f:
     data = f.read()
-rows = data.split('\n')[:-1]
+command_list = data.split('\n')[:-1]
 
-# part 1
 total_signal_strength = 0
-x = 1
+screen = ''
+X_register = 1
 cycle = 0
-for line in rows:
-    iters = 1 + int(line[0] == 'a')
+for line in command_list:
+    iters = 1 + int(line[:3] == 'add')
     for _ in range(iters):
+        screen += '#' if abs(X_register - cycle % 40) <= 1 else '.'
         cycle += 1
         if cycle % 40 == 20:
-            total_signal_strength += x*cycle
-    n = int(line.split(' ')[1]) if line[0] == 'a' else 0
-    x += n
+            total_signal_strength += X_register * cycle
+        if cycle % 40 == 0:
+            screen += '\n'
+    n = int(line.split(' ')[1]) if line[:3] == 'add' else 0
+    X_register += n
 
+# part 1
 print(total_signal_strength)
 
 # part 2
-screen = ''
-x = 1
-cycle = 0
-for line in rows:
-    iters = 1 + int(line[0] == 'a')
-    for _ in range(iters):
-        screen += '#' if abs(x - cycle % 40) <= 1 else '.'
-        cycle += 1
-        if cycle % 40 == 0:
-            screen += '\n'
-    n = int(line.split(' ')[1]) if line[0] == 'a' else 0
-    x += n
-
 print(screen)
